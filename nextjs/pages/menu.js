922e258bd04d15537bb5c7a34297bf7f53d57855
Wichayada66011293 <66011293@kmitl.ac.th>
2024-10-04 20:09:30 +0700

    b

--- nextjs/pages/menu.js
+++ nextjs/pages/menu.js
@@ -1,6 +1,50 @@
-import React from "react";
-import { Box } from "@mui/material";
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Grid from "@mui/material/Grid2";
+import { Box, Typography, Button, TextField } from "@mui/material"; // Use by LoginForm
+import useBearStore from "@/store/useBearStore";
+import React, { useState } from 'react';
 
-export default function Test() {
-  return <Box>start menu</Box>;
+function Home() {
+  const [inputText, setInputText] = useState('');
+  const [displayText, setDisplayText] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handleClick = () => {
+    setDisplayText(inputText);
+  };
+  return (
+    <Box textAlign= "center" p={4} sx={{border:1}}>
+    <Typography variant="h4" gutterBottom>
+      Start menu
+    </Typography>
+
+    <Grid container spacing={2}>
+      <Grid size={{xs: 6, md: 4}}>
+        <Button variant="contained" color="primary" href="/setting">
+          Setting
+        </Button>
+      </Grid>
+
+      <Grid size={{xs: 6, md: 4}}>
+        <Button variant="contained" color="primary" href="/login">
+          Login
+        </Button>
+      </Grid>
+
+      <Grid size={{xs: 6, md: 4}}>
+        <Button variant="contained" color="primary" href="/register">
+          Register
+        </Button>
+      </Grid>
+    </Grid>
+  </Box>
+  
+  );
 }
+
+export default Home;
