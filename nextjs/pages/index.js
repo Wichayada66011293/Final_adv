83e54f93a9564db16f33e993c293ccecb6d21292
Wichayada66011293 <66011293@kmitl.ac.th>
2024-10-04 20:52:20 +0700

    gg

---
+++ nextjs/pages/index.js
@@ -0,0 +1,15 @@
+import { Box, Typography, Button, TextField } from "@mui/material"; // Use by LoginForm
+import React, { useState } from 'react';
+
+function Home() {
+  return (
+    <Box >
+    <Typography variant="h4" gutterBottom>
+      Start
+    </Typography>
+  </Box>
+  
+  );
+}
+
+export default Home;
